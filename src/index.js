456ccffb22118fe62f4401fd4507e232debcafd0
dylan <d.hedges1@hotmail.co.uk>
2018-04-19 20:20:33 +0000

    Added React route (index)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+//BrowserRouter - an component/object that interacts with the history library and decides what to do based on the URL change (looks at entire URL), Route - a component/object that checks URL and identifies the component to be shown (can render inside of any other component)
+import { BrowserRouter, Route} from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+//<BrowserRouter> - paths (using then Route component) go inside; <Route path="" component={}/> - if a user goes to X route/URL, display X component;
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
