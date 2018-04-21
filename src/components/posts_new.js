6da4ec9124a87a0ec64f9ffd4f25b252e5c8fc6a
dylan <d.hedges1@hotmail.co.uk>
2018-04-21 13:14:14 +0000

    Added Redux-Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+//"reduxForm" - a helper that allows the component to communicate with the 'redux-form' reducer in the index.js reducers file -> the Redux store (similar to the Connect helper)
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+    //<Field> - input that will be visible to the users; name="title" - a property what piece of state this field will produce; componment={} - a prop
     render() {
         return(
-            <div>
-                PostsNew!
-            </div>
+            <form>
+                <Field
+                    name="title"
+                    componment={}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+//"reduxForm" - allows component to communicate with the Redux-Form reducer; form: 'PostsNewForm' - the name for this specific form (has to be a unique string, otherwise states from other forms with the same name will merge together), sometimes we might have multiple forms on the screen at the same time (e.g sign up and sign in) this helps to isolate different forms and their states
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+//Takes the "reducer" property from 'redux-form' and assigns it to the alias "formReducer" (this is because "reducer" is not very specific and could be used by another library causing problems, it is reccomended by Redux-Form to use an alias)
+import {reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
+//"posts: PostsReducer" - applies our Posts Reducer to the "posts" piece of state (global/app level); "form: formReducer" - applies Redux-Form to the "form" piece of state (global/app level)
 const rootReducer = combineReducers({
-    posts: PostsReducer
+    posts: PostsReducer,
+    form: formReducer
 });
 
 export default rootReducer;
